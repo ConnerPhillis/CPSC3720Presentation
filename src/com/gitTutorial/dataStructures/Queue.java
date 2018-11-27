4f433cef75e4b376cb7f671d5e2053153629c486
Conner Phillis <conner.phillis@keymark.dom>
2018-11-26 21:05:52 -0500

    Deleted doNothing funciton

--- src/com/gitTutorial/dataStructures/Queue.java
+++ src/com/gitTutorial/dataStructures/Queue.java
@@ -6,10 +6,6 @@ public class Queue<T> implements IQueue<T> {
 
     private ArrayList<T> elements = new ArrayList<>();
 
-    private void doNothing(){
-        int yolo = 0;
-    }
-
     public T dequeue(){
         return elements.remove(0);
     }
