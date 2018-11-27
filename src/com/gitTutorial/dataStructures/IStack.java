ec0fbcbfdb6dc9cac4ae3fae43399e8d0f766682
Conner Phillis <conner.phillis@keymark.dom>
2018-11-27 15:26:11 -0500

    reset example

--- src/com/gitTutorial/dataStructures/IStack.java
+++ src/com/gitTutorial/dataStructures/IStack.java
@@ -2,14 +2,8 @@ package com.gitTutorial.dataStructures;
 
 public interface IStack<T> {
 
-    /*
-    returns and removes the element that is on the top of the stack
-     */
     T pop();
 
-    /*
-    returns the top element from the stack without removing
-     */
     T peek();
 
     void push(T element);
--- src/com/gitTutorial/dataStructures/Stack.java
+++ src/com/gitTutorial/dataStructures/Stack.java
@@ -6,6 +6,10 @@ public class Stack<T> implements IStack<T> {
 
     private ArrayList<T> elements = new ArrayList<>();
 
+    private void  doNothing(){
+        int yolo = 0;
+    }
+
     public T pop(){
         return elements.remove(elements.size() - 1);
     }
