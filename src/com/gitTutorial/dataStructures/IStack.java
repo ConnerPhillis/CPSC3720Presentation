b4909ec5f8654cd8e797fb51dde5dd44b8ae52c0
Conner Phillis <conner.phillis@keymark.dom>
2018-11-26 21:21:24 -0500

    cleaned up some of the code

--- src/com/gitTutorial/dataStructures/IStack.java
+++ src/com/gitTutorial/dataStructures/IStack.java
@@ -2,8 +2,14 @@ package com.gitTutorial.dataStructures;
 
 public interface IStack<T> {
 
+    /*
+    returns and removes the element that is on the top of the stack
+     */
     T pop();
 
+    /*
+    returns the top element from the stack without removing
+     */
     T peek();
 
     void push(T element);
